0f0b8c070cd16a3189beba2fde8097a5bb0fe716
Kanchan Pandit <kanchan12oct@gmail.com>
2021-05-05 17:32:16 +0530

    Wokring on Create Account

--- src/main/java/PageObject/CreateAccount_details.java
+++ src/main/java/PageObject/CreateAccount_details.java
@@ -12,7 +12,19 @@ public class CreateAccount_details {
 	By Cust_FirstName=  By.cssSelector("input[id='customer_firstname']"); 
 	By Cust_LastName=  By.cssSelector("input[id='customer_lastname']");
 	By Create_pwd =  By.cssSelector("input[id='passwd']");
-	By DOB_Date = By.cssSelector("div[id='uniform-days']"); 
+	By DOB_Date = By.cssSelector("select[id='days']"); 
+	By DOB_Month = By.cssSelector("select[id='months']"); 
+	By DOB_Year = By.cssSelector("select[id='years']"); 
+	
+	By AddressFirstName = By.xpath("//input[@id='firstname']"); 
+	By AddressLastName  = By.xpath("//input[@id='lastname']"); 
+	By Address  = By.xpath("//input[@id='address1']"); 
+	By Addresscity	= By.xpath("//input[@id='city']"); 
+	By Addressstate = By.xpath("//select[@id='id_state']");
+	By PostCode = By.xpath("//input[@id='postcode']"); 
+	By AdditionalInfo = By.xpath("//textarea[@id='other']"); 
+	By MobilePhone = By.xpath("//input[@id='phone_mobile']"); 
+	
 	
 	public CreateAccount_details(WebDriver driver) {
 		
@@ -39,6 +51,47 @@ public class CreateAccount_details {
 		
 		public WebElement getBirthDate() {
 			 return driver.findElement(DOB_Date); 
+		}
+		
+		
+		public WebElement getBirthMonth() {
+			 return driver.findElement(DOB_Month); 
+		}
+		
+		public WebElement getBirthyear() {
+			 return driver.findElement(DOB_Year); 
+		}
+		
+		public WebElement getAddressFirstName() {
+			 return driver.findElement(AddressFirstName); 
+		}
+		
+		public WebElement getAddressLastName() {
+			 return driver.findElement(AddressLastName); 
+		}
+		
+		public WebElement getAddress() {
+			 return driver.findElement(Address); 
+		}
+		
+		public WebElement getAddressCity() {
+			 return driver.findElement(Addresscity); 
+		}
+		
+		public WebElement getAddressState() {
+			 return driver.findElement(Addressstate); 
+		}
+		
+		public WebElement getPostCode() {
+			 return driver.findElement(PostCode); 
+		}
+		
+		public WebElement getAdditonalInfo() {
+			 return driver.findElement(AdditionalInfo); 
+		}
+		
+		public WebElement getAddressPhone() {
+			 return driver.findElement(MobilePhone); 
 		}
 		
 	}
--- src/test/java/Testing_Maven/end2end_test/HomePage.java
+++ src/test/java/Testing_Maven/end2end_test/HomePage.java
@@ -23,25 +23,47 @@ public class HomePage extends base {
 	
 	//Click on SignInButton
 	CreateAccount CreatAc = lp.getSignIn(); 
-	CreatAc.getEmailCreate().sendKeys("kanchan12oct@gmail.com");
+	CreatAc.getEmailCreate().sendKeys(prop.getProperty("CreateEmail"));
 
-	
-
+	//Click on SubmitButton
 	CreateAccount_details crtAC_details = CreatAc.getSubmitButton(); 
 	crtAC_details.getGender().click();
+	
+	//Enter Customer details
 	crtAC_details.getCustomerFirstName().sendKeys("Kanchan");
 	crtAC_details.getCustomerLastName().sendKeys("Pandit");
 	crtAC_details.getCreatePassword().sendKeys("Blackrose");
 	
 	
-	
 	Select dropdown = new Select (crtAC_details.getBirthDate());
-	dropdown.selectByIndex(11);	
+	dropdown.selectByValue("12");
+	System.out.println(dropdown.getFirstSelectedOption().getText());
+	
+	
+	Select dropdown1 = new Select (crtAC_details.getBirthMonth());
+	dropdown1.selectByIndex(10);
+	System.out.println(dropdown1.getFirstSelectedOption().getText());
+	
+	Select dropdown2 = new Select (crtAC_details.getBirthyear());
+	dropdown2.selectByValue("1990");
+	System.out.println(dropdown2.getFirstSelectedOption().getText());
+	
+	
+	//Enter Customer Address Details
+	crtAC_details.getAddressFirstName().sendKeys("Kanchan");
+	crtAC_details.getAddressLastName().sendKeys("Pandit");
+	crtAC_details.getAddress().sendKeys("RoseMary Roadm, HouseNo-12 ");
+	crtAC_details.getAddressCity().sendKeys("NewDelhi");
+	
+	Select dropdown3 = new Select (crtAC_details.getAddressState());
+	dropdown3.selectByIndex(5);
+	System.out.println(dropdown3.getFirstSelectedOption().getText());
+	
+	crtAC_details.getPostCode().sendKeys("BX4 102");
+	crtAC_details.getAdditonalInfo().sendKeys("Hello, Please send the delivery after 0900 AM");
+	crtAC_details.getAddressPhone().sendKeys("95999123459");
 	
 	}
 	
-	
-	
-
 	
 }
